93b882595463b4ddbcd5e44d8ac3390b3aa3b5e4
Adam Zdebel <zdebcio97@gmail.com>
2020-11-23 21:30:57 +0100

    Add starts assets

---
+++ footer.php
@@ -0,0 +1,51 @@
+<footer>
+    <div class="pos-center">
+        <section class="menu">
+            <ul>
+                <li><a href="#">Pomidor</a></li>
+                <li><a href="#">Pietruszka</a></li>
+                <li><a href="#">Makaron</a></li>
+                <li><a href="#">Warzywa</a></li>
+                <li><a href="#">Kurczak</a></li>
+            </ul>
+            <ul>
+                <li><a href="#">Kuchnia japońska</a></li>
+                <li><a href="#">Włoskie smaki</a></li>
+                <li><a href="#">Hiszpańskie tapas</a></li>
+                <li><a href="#">Kuchnia tajska</a></li>
+                <li><a href="#">Kuchnia francuska</a></li>
+            </ul>
+            <ul>
+                <li><a href="#">Lunch</a></li>
+                <li><a href="#">Przystawka</a></li>
+                <li><a href="#">śniadanie</a></li>
+                <li><a href="#">Obiad</a></li>
+                <li><a href="#">Deser</a></li>
+            </ul>
+        </section>
+
+        <section class="tag-cloud">
+            <a href='#' title='2 tematy' style='font-size: 16.5px;'>kurczak</a>
+            <a href='#' title='1 temat' style='font-size: 11px;'>makaron</a>
+            <a href='#' title='1 temat' style='font-size: 13px;'>pietruszka</a>
+            <a href='#' title='1 temat' style='font-size: 11px;'>pomidor</a>
+            <a href='#' title='2 tematy' style='font-size: 16.5px;'>sałata</a>
+            <a href='#' title='1 temat' style='font-size: 10px;'>warzywa</a>
+            <a href='#' title='1 temat' style='font-size: 13px;'>pietruszka</a>
+            <a href='#' title='2 tematy' style='font-size: 16.5px;'>kurczak</a>
+            <a href='#' title='1 temat' style='font-size: 11px;'>makaron</a>
+            <a href='#' title='1 temat' style='font-size: 11px;'>pomidor</a>
+            <a href='#' title='2 tematy' style='font-size: 16.5px;'>kurczak</a>
+            <a href='#' title='1 temat' style='font-size: 11px;'>makaron</a>
+            <a href='#' title='1 temat' style='font-size: 13px;'>pietruszka</a>
+        </section>
+    </div>
+</footer>
+<script src="<?php echo get_theme_file_uri('/js/jquery-1.9.1.min.js') ?>"></script>
+<script src="<?php echo get_theme_file_uri('/js/jquery.carouFredSel-6.2.0.js') ?>"></script>
+<script src="<?php echo get_theme_file_uri('/js/jquery.jqtransform.js') ?>"></script>
+<script src="<?php echo get_theme_file_uri('/js/slides.jquery.js') ?>"></script>
+<script src="<?php echo get_theme_file_uri('/js/scripts.js') ?>"></script>
+</body>
+
+</html>
